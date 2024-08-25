1086b7b6e4f4da56ceda115dfca015f031f77b62
SeLvAkUmAr-Subramanian <selvakumarsubramaniant@gmail.com>
2024-08-25 11:50:39 +0530

    Updated Code

--- GitHubIntegration/src/GitHubDirectIntegration.java
+++ GitHubIntegration/src/GitHubDirectIntegration.java
@@ -2,7 +2,7 @@
 public class GitHubDirectIntegration {
 
 	public static void main(String[] args) {
-		System.out.println("Eclipse Git Hub Integration");
+		System.out.println("Today Is August 24");
 
 	}
 
